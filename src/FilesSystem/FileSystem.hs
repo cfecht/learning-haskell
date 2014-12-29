0fda85e7c9263f9aa155e88c39ca547b049013eb
Christian Fecht <christian.fecht@sap.com>
2014-12-29 18:10:30 +0100

    Update to FileSystem.hs

--- src/FilesSystem/FileSystem.hs
+++ src/FilesSystem/FileSystem.hs
@@ -1,5 +1,6 @@
 module FileSystem where
 
+import Data.Maybe (isJust, isNothing)
 import Control.Monad (forM)
 import System.Directory (doesDirectoryExist, getDirectoryContents)
 import System.FilePath ((</>))
@@ -7,74 +8,101 @@ import System.IO
 import Control.Exception (IOException, handle, bracket)
 import qualified Data.Map as Map 
 import Data.List (sortBy)
+import Crypto.Hash.SHA1 (hashlazy)
+import qualified Data.ByteString as Strict
+import qualified Data.ByteString.Lazy as Lazy
+import Text.Printf (printf)
 
-data FileInfo = FileInfo { paths :: [FilePath] }
+type FileHash = Strict.ByteString
+type FileSize = Integer
 
-getRecursiveContents :: FilePath -> IO [FilePath] -> IO [FilePath]
+data IdenticalFiles = IdenticalFiles FileSize FileHash [FilePath]
 
-getRecursiveContents topdir paths = do
-  names  <- handle returnEmptyList $ getDirectoryContents topdir
-  f names paths
-  where 
-      f [] paths_ = paths_
-      f (p:ps) paths_ = do 
-                           f ps (addDirectoryEntry p paths_)
-      addDirectoryEntry "."   ps = ps
-      addDirectoryEntry ".."  ps = ps 
-      addDirectoryEntry entry ps = do
-           let currentPath = topdir </> entry
-           isDirectory <- doesDirectoryExist currentPath
-           if isDirectory
-               then getRecursiveContents currentPath ps
-               else do 
-                      ps_ <- ps
-                      return (currentPath : ps_)
-  
+insert :: FilePath -> FileSize -> FileHash -> [IdenticalFiles] -> [IdenticalFiles]
+insert path size hash [] = [IdenticalFiles size hash [path]]
+insert path size hash ((IdenticalFiles size' hash' paths) : ids) = 
+    if size == size' && hash == hash'
+        then (IdenticalFiles size hash (path : paths)) : ids
+        else (IdenticalFiles size' hash' paths) : insert path size hash ids
+
+data State = State {
+               paths :: [FilePath],
+               numberOfFiles :: Integer,
+               equalSizedFiles :: Map.Map Integer [FilePath]
+             } deriving (Show)
+             
+initialState :: State
+initialState = State {
+                 paths = [],
+                 numberOfFiles = 0,
+                 equalSizedFiles = Map.empty
+               }
+             
+addPath :: FilePath -> FileSize -> FileHash-> State -> State
+addPath path size hash state =
+    State {
+       paths = path : (paths state)
+     , numberOfFiles = (numberOfFiles state) + 1
+     , equalSizedFiles = Map.insertWith (++) size [path] (equalSizedFiles state)
+    }
+
+foldM' :: Monad m => [a] -> (a -> m b -> m b) -> m b -> m b
+foldM' [] f = id
+foldM' (x : xs) f = \s -> foldM' xs f (f x s)
+
+getJust :: Maybe a -> a
+getJust (Just v) = v
+
+addDirectoryEntry :: FilePath -> IO State -> IO State
+addDirectoryEntry path state = do
+    isDirectory <- doesDirectoryExist path
+    if isDirectory
+        then getRecursiveContents path state
+        else do 
+              fileSize <- getFileSize path
+              hash <- hashFile path  
+              if isJust fileSize && isJust hash
+                  then fmap (addPath path (getJust fileSize) (getJust hash)) state
+                  else state
+         
+                  
+getRecursiveContents :: FilePath -> IO State -> IO State
+getRecursiveContents path state = do
+  names  <- handle returnEmptyList $ getDirectoryContents path
+  let paths = map ((</>) path) (filter (`notElem` [".", ".."]) names)
+  foldM' paths addDirectoryEntry state
+   
 returnEmptyList :: IOException -> IO [FilePath]
 returnEmptyList _ = return []
  
+getFileSize :: FilePath -> IO (Maybe Integer)
 getFileSize path = handle doNothing $
   bracket (openFile path ReadMode) hClose $ \h -> do
     size <- hFileSize h
     return (Just size)  
 
-doNothing :: IOException -> IO (Maybe Integer)
-doNothing _ = return Nothing    
+   
+hashFile :: FilePath -> IO (Maybe Strict.ByteString)
+hashFile path = handle doNothing $ (fmap (Just . hashlazy) . Lazy.readFile ) path
 
-getFileSizes :: FilePath -> IO [(FilePath, Integer)]
-getFileSizes path = do
-    paths <- getRecursiveContents path (return [])
-    f paths
-    where f [] = return []
-          f (p:ps) = do 
-                       size <- getFileSize p
-                       sizes <- f ps
-                       case size of
-                         Nothing -> return sizes
-                         Just s  -> return ((p, s) : sizes)
-    
+doNothing :: IOException -> IO (Maybe a)
+doNothing e = do
+    putStrLn $ show e
+    return Nothing    
+
 
 testPath :: FilePath
 testPath = "C:\\Users\\D025630\\Documents"
--- testPath = "C:\\HANA_Studio"
+--testPath = "C:\\HANA_Studio"
 
-createFileSizeHistogram :: [(FilePath, Integer)] -> Map.Map Integer Integer
-createFileSizeHistogram [] = Map.empty
-createFileSizeHistogram ((p,s) : r) = 
-    case Map.lookup s m of
-      Nothing -> Map.insert s 1 m
-      Just v' -> Map.insert s (v'+1) m
-    where m = createFileSizeHistogram r
-    
-orderPairBySecondDesc :: (Integer, Integer) -> (Integer, Integer) -> Ordering
-orderPairBySecondDesc (_, b1) (_, b2) 
-   | b1 < b2  = GT
-   | b1 == b2 = EQ
-   | b1 > b2  = LT
-         
+       
 main :: IO ()
 main = do 
-        sizes <- getFileSizes testPath
-        let histo = createFileSizeHistogram sizes
-            al    = Map.toList histo
-        putStrLn (show (sortBy orderPairBySecondDesc al))
+         state <- getRecursiveContents testPath (return initialState)
+         putStrLn $ show (numberOfFiles state)
+         
+
+toHex :: Strict.ByteString -> String
+toHex bytes = Strict.unpack bytes >>= printf "%02x"
+
+
